3bb8f54898a24e4fa5bc79714170f1647b7cf581
関根大輔 <sekinedaisuke@sekinedaisukenoMacBook-Air.local>
2026-08-26 20:03:53 +0900

    ログイン機能のSpring Security設定修正

--- src/main/java/com/spring/springbootapplication/config/SecurityConfig.java
+++ src/main/java/com/spring/springbootapplication/config/SecurityConfig.java
@@ -1,6 +1,6 @@
-    package com.spring.springbootapplication.config;
+package com.spring.springbootapplication.config;
 
-    import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,7 +31,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
                     "/",
                     "/users/register",
                     "/login",
-                    "/top",
                     "/error", 
                     "/css/**",
                     "/js/**")
